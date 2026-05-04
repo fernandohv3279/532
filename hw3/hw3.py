b9fbfa421015a7a5e09af34bcd8ed7af9451b826
Fernando Herrera <cafeherval@gmail.com>
2026-05-04 12:35:50 -0600

    wip augmented matrices

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -20,7 +20,7 @@ Row1=np.concatenate([np.identity(n),-np.identity(n),np.zeros((n,m)),np.zeros((n,
 # Make "row" 2 of the big matrix
 Row2=np.concatenate([-np.identity(n),-np.identity(n),np.zeros((n,m)),np.zeros((n,1))], axis=1)
 
-# Make "row" 2 of the big matrix
+# Make "row" 3 of the big matrix
 XY= Y @ X
 Zmn=np.zeros((m,n))
 Im=np.identity(m)
@@ -36,9 +36,23 @@ cost=np.hstack([np.zeros(n),np.ones(n),c*np.ones(m),0])
 # Make h vector
 
 h=np.hstack([np.zeros(n),np.zeros(n),np.ones(m)])
+# print(A.shape)     #(629,630)
+# print(cost.shape)  #(630,)
+# print(h.shape)     #(629,)
+
+# Add more inequalities
+
+Row4=np.hstack([np.zeros((n,n)),-np.identity(n),np.zeros((n,m+1))])
+# print(Row4.shape)
+
+Row5=np.hstack([np.zeros((m,2*n)),-np.identity(m),np.zeros((m,1))])
+# print(Row5.shape)
+
+A_big=np.vstack([A,Row4,Row5])
+print(A_big.shape)
 print(A.shape)
-print(cost.shape)
-print(h.shape)
+
+h_big=np.hstack([h,np.zeros(n),np.zeros(m)])
 
 # Solve linear program
 
@@ -47,10 +61,10 @@ print(h.shape)
   #             A_eq @ x == b_eq
   #             bounds on x
 
-b_ub = h
 bounds = [(0, None)] * len(cost)
 
-result = linprog(cost, A_ub=A, b_ub=b_ub, bounds=bounds)
+result = linprog(cost, A_ub=A_big, b_ub=h_big, bounds=bounds)
 
 print(result.x)      # optimal solution
-print(result.fun)    # optimal objective value
+# print(len(result.x))      # optimal solution
+# print(result.fun)    # optimal objective value

6b664609ac4c0060be8d27113e29272cf19b4cd2
Fernando Herrera <cafeherval@gmail.com>
2026-05-07 14:03:56 -0600

    change labels to -1 and 1

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -6,6 +6,7 @@ data = scipy.io.loadmat('wisconsin_breast_cancer.mat')
 
 X = data['X']                                          # (569, 30)
 y = data['y']                                          # (569, 1)
+y = np.where(y == 0, -1, 1)                           # convert 0/1 → -1/+1
 Y=np.diag(y.flatten())
 m = X.shape[0]
 n = X.shape[1]
@@ -24,35 +25,21 @@ Row2=np.concatenate([-np.identity(n),-np.identity(n),np.zeros((n,m)),np.zeros((n
 XY= Y @ X
 Zmn=np.zeros((m,n))
 Im=np.identity(m)
-Row3=-np.concatenate([XY,Zmn,Im,y], axis=1)
+Row3=-np.concatenate([XY,-XY,Im,y], axis=1)
 
 # Assemble big matrix
 A=np.vstack([Row1,Row2,Row3])
 
 # Make cost vector
 c=1 # Change this later
-cost=np.hstack([np.zeros(n),np.ones(n),c*np.ones(m),0])
+cost=np.hstack([np.ones(n),np.ones(n),c*np.ones(m),0])
 
 # Make h vector
 
-h=np.hstack([np.zeros(n),np.zeros(n),np.ones(m)])
+h=np.hstack([np.zeros(n),np.zeros(n),-np.ones(m)])
 # print(A.shape)     #(629,630)
 # print(cost.shape)  #(630,)
 # print(h.shape)     #(629,)
-
-# Add more inequalities
-
-Row4=np.hstack([np.zeros((n,n)),-np.identity(n),np.zeros((n,m+1))])
-# print(Row4.shape)
-
-Row5=np.hstack([np.zeros((m,2*n)),-np.identity(m),np.zeros((m,1))])
-# print(Row5.shape)
-
-A_big=np.vstack([A,Row4,Row5])
-print(A_big.shape)
-print(A.shape)
-
-h_big=np.hstack([h,np.zeros(n),np.zeros(m)])
 
 # Solve linear program
 
@@ -63,8 +50,17 @@ h_big=np.hstack([h,np.zeros(n),np.zeros(m)])
 
 bounds = [(0, None)] * len(cost)
 
-result = linprog(cost, A_ub=A_big, b_ub=h_big, bounds=bounds)
+result = linprog(cost, A_ub=A, b_ub=h, bounds=bounds)
 
-print(result.x)      # optimal solution
+# print(result.x)      # optimal solution
 # print(len(result.x))      # optimal solution
 # print(result.fun)    # optimal objective value
+
+w = result.x[:n] - result.x[n:2*n]
+z = result.x[2*n:2*n+m]
+t = result.x[-1]
+
+print("w:", w)
+print("nonzero weights:", np.sum(w != 0))
+print("bias t:", t)
+print("misclassified:", np.sum(z > 0))
